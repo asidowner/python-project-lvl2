f59b174e22eaaa8cd9603773d5e68399f0031517
Anton Sidorenkov <asid.owner@gmail.com>
2022-07-31 21:44:39 +0400

    fix tests bug #2

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,7 +5,7 @@ from gendiff.lib.diff import diff
 
 def generate_diff(first_file_path: str,
                   second_file_path: str,
-                  format_: str) -> str:
+                  format_: str = 'stylish') -> str:
     first_file_data: dict = parse_file(first_file_path)
     second_file_data: dict = parse_file(second_file_path)
 
